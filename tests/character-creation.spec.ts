d3171f97a371dc6425bf7e0fe83a99baf0e216d0
Thibaud Descoins <thibaud.descoins@eneco.com>
2025-10-05 16:16:23 +0200

    adding beforeEach + update smoketests

--- tests/character-creation.spec.ts
+++ tests/character-creation.spec.ts
@@ -3,6 +3,17 @@ import { HomePage } from '../pages/homepage';
 import { GamePage } from '../pages/gamepage';
 import { EXPECTED_STATS } from '../utils/test-data';
 
+let home: HomePage;
+let game: GamePage;
+
+test.beforeEach(async ({ page }) => {
+  home = new HomePage(page);
+  game = new GamePage(page);
+
+  await home.openAppAndCheckHomePage();
+});
+
+test.describe('Character creation flows', () =>{
 test('create new Knight and verify stats update before starting', async ({ page }) => {
   const home = new HomePage(page);
   const game = new GamePage(page);
@@ -16,4 +27,36 @@ test('create new Knight and verify stats update before starting', async ({ page 
   await home.createCharacter(name);
   await game.assertCharacterHeader(name, 1, build);
   await home.assertStats(EXPECTED_STATS[build]);
+});
+
+test('create new Mage and verify stats update before starting', async ({ page }) => {
+  const home = new HomePage(page);
+  const game = new GamePage(page);
+
+  const build = 'Mage';
+  await home.openAppAndCheckHomePage();
+  await home.verifyOrSelectBuild(build, true);
+  await home.assertStats(EXPECTED_STATS[build]);
+
+  const name = 'Magus';
+  await home.createCharacter(name);
+  await game.assertCharacterHeader(name, 1, build);
+  await home.assertStats(EXPECTED_STATS[build]);
+});
+
+test('create new Brigadier and verify stats update before starting', async ({ page }) => {
+  const home = new HomePage(page);
+  const game = new GamePage(page);
+
+  const build = 'Brigadier';
+  await home.openAppAndCheckHomePage();
+  await home.verifyOrSelectBuild(build, true);
+  await home.assertStats(EXPECTED_STATS[build]);
+
+  const name = 'SgtPrice';
+  await home.createCharacter(name);
+  await game.assertCharacterHeader(name, 1, build);
+  await home.assertStats(EXPECTED_STATS[build]);
+});
+
 });
