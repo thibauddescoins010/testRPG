4790be0845410ccf3e3b196c82d53d30c3fb045a
Thibaud Descoins <thibaud.descoins@eneco.com>
2025-10-05 16:16:53 +0200

    update smoketests

--- tests/smoketests.spec.ts
+++ tests/smoketests.spec.ts
@@ -3,24 +3,24 @@ import { HomePage } from '../pages/homepage';
 import { GamePage } from '../pages/gamepage';
 import { EXPECTED_STATS } from '../utils/test-data';
 
+let home: HomePage;
+let game: GamePage;
 
-test.describe('Smoke Tests', () =>{
-test('homepage has correct locators and Start button', async ({ page }) => {
-  const home = new HomePage(page);
+test.beforeEach(async ({ page }) => {
+  home = new HomePage(page);
+  game = new GamePage(page);
+
   await home.openAppAndCheckHomePage();
 });
 
-test('opens on Thief build with correct default stats', async ({ page }) => {
-  const home = new HomePage(page);
+test.describe('Smoke Tests', () =>{
+test('opens on Thief build with correct default stats', async ({}) => {
   const build = 'Thief'
-  await home.openAppAndCheckHomePage();
   await home.verifyOrSelectBuild(build);
   await home.assertStats(EXPECTED_STATS[build]);
 });
 
-test('login flow – invalid + valid login attempts', async ({ page }) => {
-  const home = new HomePage(page);
-  await home.openAppAndCheckHomePage();
+test('login flow – invalid + valid login attempts', async ({}) => {
   await home.openLoginModal();
 
   // Negative flow: submit without credentials
@@ -31,10 +31,7 @@ test('login flow – invalid + valid login attempts', async ({ page }) => {
 });
 
 test('character creation flow – missing name then valid creation', async ({ page }) => {
-  const home = new HomePage(page);
   const game = new GamePage(page);
-
-  await home.openAppAndCheckHomePage();
 
   // Negative flow — try to start without entering a name
   await home.tryStartWithoutName();
